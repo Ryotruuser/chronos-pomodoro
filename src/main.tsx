f0e2834085fc70184d3be5b574e28609816984a4
Ryotruuser <ryonantruuser@gmail.com>
2025-10-23 18:37:55 -0300

    reactfragments,exports e afins

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+const nome = prompt('Digite seu nome: ');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App children={nome}></App>
   </StrictMode>,
-)
+);
