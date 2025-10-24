f77526e6ecd783e3f3dda3e66eae4d45f3d763b8
Ryotruuser <ryonantruuser@gmail.com>
2025-10-24 11:14:11 -0300

    entedendo css modules'

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Heading } from './components/Heading';
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
   return (
     <>
-      <h1>Olá mundo!</h1>
+      <Heading />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem at
         iure, quasi eveniet dolores expedita eaque facere, laudantium error
