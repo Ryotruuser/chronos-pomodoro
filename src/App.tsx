72c15111ba92c2decc02b7b0a23497af3dd29bfe
Ryotruuser <ryonantruuser@gmail.com>
2025-10-30 18:21:21 -0300

    estrutura react

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-import { Heading } from './components/Heading';
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem at
-        iure, quasi eveniet dolores expedita eaque facere, laudantium error
-        necessitatibus ea sequi beatae quibusdam itaque quam autem ab
-        perspiciatis quas.
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
